9015627e6239a76c771d3925ab9fb5d456a7edfa
Pavel Vashkel <p.vashkel@datarockets.com>
2021-03-07 10:32:44 +0100

    Load basic WebView with DeepL

--- app/src/main/java/com/vshkl/deeplweb/WebViewActivity.kt
+++ app/src/main/java/com/vshkl/deeplweb/WebViewActivity.kt
@@ -1,9 +1,28 @@
 package com.vshkl.deeplweb
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
-class WebViewActivity : AppCompatActivity(R.layout.activity_web_ciew) {
+class WebViewActivity : AppCompatActivity(R.layout.activity_web_view) {
 
+    private lateinit var wvMain: WebView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        wvMain = findViewById(R.id.wv_main)
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        wvMain.loadUrl(getString(R.string.url_main))
+        wvMain.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+    }
 
 }
